dd79bb131eb8b41971a97a53d50f9e605d66b409
alisalahTDS <Angular@tamauzeds.com>
2022-02-22 10:30:12 +0200

    12

--- src/app/teams/view-teams/view-teams.component.ts
+++ src/app/teams/view-teams/view-teams.component.ts
@@ -43,7 +43,7 @@ export class ViewTeamsComponent implements OnInit {
     })
 
     dialogRef.afterClosed().subscribe((result: string) => {
-      if (result === 'DONE') {
+      if (result) {
         this.getAllTeams()
       }
     })
